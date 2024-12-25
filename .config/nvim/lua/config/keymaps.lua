62ac19d2960723a18980aa92ef81d93502647e39
LuytBui <luytbq43@gmail.com>
2024-12-25 17:17:21 +0700

    update nvim config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
 
-vim.keymap.set({"n", "i", "v"}, "<c-c>", "<cmd>noh<cr><esc>")
+local map = vim.keymap.set
+local del = vim.keymap.del
+
+map({"n", "i", "v"}, "<c-c>", "<cmd>noh<cr><esc>")
+map({"n"}, "<leader>va", "ggVG")
+
+-- Change paste behavior
+map({"v"}, "p", "pgvy")
+
+-- Terminal Mappings
+del({"n", "t"}, "<c-/>")
+del({"n", "t"}, "<c-_>") -- <c-/> is translated to <c-_> in some terminal
+map("n", "<c-`>",      function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
+map("t", "<c-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+-- map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
 
 -- open terminal
 -- vim.keymap.set("n", "<C-S-`>", function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
